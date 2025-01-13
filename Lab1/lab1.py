8e8935357ec24b74c8dde00d397b52f07b93dcf7
hcustod <111096105+hcustod@users.noreply.github.com>
2025-01-12 23:46:01 -0500

    Add: Lab 1

---
+++ Lab1/lab1.py
@@ -0,0 +1,18 @@
+# Sample Coding Questions 01 Week 01
+# Henrique Custodio
+
+arr = [1, 4, 7, 9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = a - ((b ** c) // d) + (a % c)
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Please enter your age: "))
+userAge += 22
+print("Now showing the shop items filtered by age: {}".format(userAge))
